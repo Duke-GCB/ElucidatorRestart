616cd641ed8c24ed4f3dc834af68bfecbbc485a7
John Bradley <johnbradley2008@gmail.com>
2018-01-17 12:54:55 -0500

    remove authdigest_shm files on startup

--- start-apache.sh
+++ start-apache.sh
@@ -6,6 +6,9 @@ if [ -e /var/run/httpd/httpd.pid ]; then
   rm -f /var/run/httpd/httpd.pid
 fi
 
+# Remove shared memory files that prevent apache from starting
+rm -f /var/run/httpd/authdigest_shm.*
+
 # Run apache in foreground
 echo "Starting httpd"
 /usr/sbin/apachectl -DFOREGROUND
